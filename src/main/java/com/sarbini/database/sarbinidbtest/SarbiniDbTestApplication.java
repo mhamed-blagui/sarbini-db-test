836abeb78bf1fb657d844aaa0846a1eef0f59cf6
mhamed-blagui <mhamed.blagui88@gmail.com>
2018-08-19 22:25:52 +0100

    Master

--- src/main/java/com/sarbini/database/sarbinidbtest/SarbiniDbTestApplication.java
+++ src/main/java/com/sarbini/database/sarbinidbtest/SarbiniDbTestApplication.java
@@ -1,7 +1,12 @@
 package com.sarbini.database.sarbinidbtest;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SarbiniDbTestApplication {
@@ -9,4 +14,12 @@ public class SarbiniDbTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SarbiniDbTestApplication.class, args);
 	}
+	
+	@Value("${db.port}")
+    private String h2DbPort;
+
+    @Bean
+    public Server server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", h2DbPort).start();
+    }
 }
